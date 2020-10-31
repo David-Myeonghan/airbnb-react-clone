5b0dcd5640c427ceb8c7f5f84068ed8da92d108d
David MH Ryu <myeonghan12@gmail.com>
2020-10-31 17:49:47 +1000

    not finish but deploy

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -8,7 +9,9 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 function Header() {
 	return (
 		<div className="header">
-			<img className="header__icon" src="https://miro.medium.com/max/770/0*NChTo-XqLOxLabIW" />
+			<Link to="/">
+				<img className="header__icon" src="https://miro.medium.com/max/770/0*NChTo-XqLOxLabIW" />
+			</Link>
 
 			<div className="header__center">
 				<input type="text" />
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,13 +1,49 @@
 import React from "react";
 import "./SearchPage.css";
+import Button from "@material-ui/core/Button";
+import SearchResult from "./SearchResult";
 
 function SearchPage() {
 	return (
 		<div className="searchPage">
 			<div className="searchPage__info">
 				<p>62 days ﹒ 1 November to 30 November ﹒ 2 guest </p>
-				<h1>Stays nearby</h1>
+				<h1>Places to stay near you</h1>
+				<Button variant="outlined">Cancellation Flexibility</Button>
+				<Button variant="outlined">Type of place</Button>
+				<Button variant="outlined">Price</Button>
+				<Button variant="outlined">Rooms and beds</Button>
+				<Button variant="outlined">More filters</Button>
 			</div>
+			<SearchResult
+				img="https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+				location="Private room in centre of Brisbane"
+				title="Stay at this spacious House"
+				description="1 guest ﹒ 1 bedroom ﹒ Queenbed ﹒ Private bathroom ﹒ Wifi ﹒Kitchen ﹒ Free parking ﹒ Washing Machine"
+				star={4.9}
+				price="$80 / night"
+				total="$2700 total"
+			/>
+
+			<SearchResult
+				img="https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+				location="Private room in centre of Brisbane"
+				title="Stay at this spacious House"
+				description="1 guest ﹒ 1 bedroom ﹒ Queenbed ﹒ Private bathroom ﹒ Wifi ﹒Kitchen ﹒ Free parking ﹒ Washing Machine"
+				star={4.9}
+				price="$80 / night"
+				total="$2700 total"
+			/>
+
+			<SearchResult
+				img="https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+				location="Private room in centre of Brisbane"
+				title="Stay at this spacious House"
+				description="1 guest ﹒ 1 bedroom ﹒ Queenbed ﹒ Private bathroom ﹒ Wifi ﹒Kitchen ﹒ Free parking ﹒ Washing Machine"
+				star={4.9}
+				price="$80 / night"
+				total="$2700 total"
+			/>
 		</div>
 	);
 }
